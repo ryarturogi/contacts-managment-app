d697c0586cd621c189abe0f1d8f0d2d904da84fe
Ricardo Guillen I <ricardo.guillen@elaniin.com>
2023-02-14 01:31:04 -0400

    ♻️ Update root files, add global notifier
    ✨ Add Toastify library to handle the in-browser notification/alert.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
+import DefaultLayout from '@/components/layout/DefaultLayout'
+import { wrapper } from '@/lib/store'
 import type { AppProps } from 'next/app'
-import { wrapper } from '../lib/store'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DefaultLayout>
+      <Component {...pageProps} />
+      <ToastContainer
+        position='top-right'
+        autoClose={2000}
+        hideProgressBar={true}
+        pauseOnHover={false}
+      />
+    </DefaultLayout>
+  )
 }
 export default wrapper.withRedux(App)
